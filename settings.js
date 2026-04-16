583e9f9767fa4c74eda1a3e8c0b749b10df1d946
stanytz378 <stanytz378@gmail.com>
2026-04-17 02:59:25 +0300

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,17 @@
+// settings.js
+const settings = {
+    packname: 'MIA KHALIFA',
+    author: 'STANY TZ',
+    botName: "MIA KHALIFA",
+    botOwner: 'STANY TZ',
+    ownerNumber: '255787079580',
+    giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+    commandMode: "public",
+    maxStoreMessages: 20,
+    storeWriteInterval: 10000,
+    description: "WhatsApp bot by STANY TZ",
+    version: "2.0.0",
+    updateZipUrl: "",
+};
+
+module.exports = settings;
